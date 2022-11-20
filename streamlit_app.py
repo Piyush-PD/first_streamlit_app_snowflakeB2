b3cd03b4f30d6262102590b890f76053f324e0f8
Piyush PD <59759166+Piyush-PD@users.noreply.github.com>
2022-11-21 01:11:57 +0530

    new menu listing

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,7 @@ streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach and Rocket Smmothie")
 streamlit.text("🐔 Hard Boiled Free-Ranged eggs")
 streamlit.text("🥑🍞 Avacado Toast")
+
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
