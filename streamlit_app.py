3dd059ab003608ddb1774bf4ec746316852eebe8
Piyush PD <59759166+Piyush-PD@users.noreply.github.com>
2022-11-21 01:15:54 +0530

    changing the index of DF to fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,11 @@ streamlit.text("🥑🍞 Avacado Toast")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 data = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+data.set_index("fruits")
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 streamlit.dataframe(data)
 
 
