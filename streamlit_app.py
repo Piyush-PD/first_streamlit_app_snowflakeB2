1fa371de979a91d2b455c3553da0f507bf355a19
Piyush PD <59759166+Piyush-PD@users.noreply.github.com>
2022-11-21 18:33:16 +0530

    changing format to json

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,7 @@ streamlit.dataframe(filter_data)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
-
+streamlit.text(fruityvice_response.json())
 
 
 
