b4c3cf12ce48a04ab1fc4a1d457a79d8583b8116
Piyush PD <59759166+Piyush-PD@users.noreply.github.com>
2022-11-22 14:30:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas as pd
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title("My Mom's new Healthy Dinner")
 " BreakFast Favourites"
@@ -35,7 +36,7 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
@@ -46,4 +47,7 @@ streamlit.dataframe(my_data_row)
 streamlit.text("What food you like to have ?")
 add_my_fruit = streamlit.text_input("What fruit would you like to have?")
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+
+my_cur.execute("insert into fruit_loadList values ('from streamlit')")
 
